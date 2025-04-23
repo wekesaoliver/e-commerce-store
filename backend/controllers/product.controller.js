4cf71fc12ce2f98d2886378c0f04329b17376ff4
wekesaoliver <oliverwekesa361@gmail.com>
2025-04-23 18:00:16 +0300

    update backend

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,7 @@
 import Product from "../models/product.model.js";
+import { getAllProducts } from "./product.controller";
+import redis from "../lib/redis.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getAllProducts = async (req, res) => {
     try {
@@ -6,6 +9,91 @@ export const getAllProducts = async (req, res) => {
         res.json({ products });
     } catch (error) {
         console.log("Error in getAllProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let featuredProducts = await redis.get("featured_products");
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));
+        }
+
+        // if not in redis, fetch from mongodb
+        // .lean() is gonna return a plain javascript object instead of mongodb document
+        // which is good for performance
+        featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+        if (!featuredProducts) {
+            return res
+                .status(404)
+                .json({ message: "No featured products found" });
+        }
+
+        // store in redis for future access
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+        res.json({ featuredProducts });
+    } catch (error) {
+        console.log("Error in getFeaturedProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+export const createProduct = async (req, res) => {
+    try {
+        const { name, description, price, image, category } = req.body;
+
+        let cloudinaryResponse = null;
+
+        if (image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image, {
+                folder: "products",
+            });
+        }
+
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url
+                ? cloudinaryResponse.secure_url
+                : "",
+            category,
+        });
+
+        res.status(201).json({ product });
+    } catch (error) {
+        console.log("Error in createProduct controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: "product not found" });
+        }
+
+        if (product.image) {
+            const publicId = product.image.split("/").pop().split(".")[0];
+
+            try {
+                await cloudinary.uploader.destroy(`products/${publicId}`);
+                console.log("deleted image from cloudinary");
+            } catch (error) {
+                console.log("Error deleting image from cloudinary", error);
+            }
+        }
+
+        await Product.findByIdAndDelete(req.params.id);
+
+        res.json({ message: "Product deleted successfully" });
+    } catch (error) {
+        console.log("Error in deleteProduct controller", error.message);
         res.status(500).json({ message: "Server error", error: error.message });
     }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+    getAllProducts,
+    getFeaturedProducts,
+    createProduct,
+    deleteProduct,
+} from "../controllers/product.controller.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 
+router.get("/featured", getFeaturedProducts);
+
+router.post("/", protectRoute, adminRoute, createProduct);
+
+router.post("/", protectRoute, adminRoute, deleteProduct);
 export default router;
